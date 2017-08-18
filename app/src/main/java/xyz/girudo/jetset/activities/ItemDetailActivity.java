b8f2133b601fc5ff185fe1d2f132d3d1bf90d0e6
Novyandi Nurahmad <novyandi.jtk07@gmail.com>
2017-08-18 10:24:48 +0700

    add item detail object  & update gradle depedencies

---
+++ app/src/main/java/xyz/girudo/jetset/activities/ItemDetailActivity.java
@@ -0,0 +1,37 @@
+package xyz.girudo.jetset.activities;
+
+import android.os.Bundle;
+
+import xyz.girudo.jetset.R;
+
+/**
+ * Created by Novyandi Nurahmad on 7/10/17
+ */
+
+public class ItemDetailActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    public void setUICallbacks() {
+
+    }
+
+    @Override
+    public int getLayout() {
+        return R.layout.activity_detail;
+    }
+
+    @Override
+    public void updateUI() {
+
+    }
+}
